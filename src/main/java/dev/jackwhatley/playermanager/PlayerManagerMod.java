724e7e043b94c56a53ef35d194f74ea7f950698c
jack-whatley <wickselm2005@gmail.com>
2024-08-24 23:51:38 +0100

    Working Kick

---
+++ src/main/java/dev/jackwhatley/playermanager/PlayerManagerMod.java
@@ -0,0 +1,72 @@
+package dev.jackwhatley.playermanager;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(PlayerManagerMod.MODID)
+public class PlayerManagerMod
+{
+    public static final String MODID = "jackwhatleyserverplayermanager";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public PlayerManagerMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.logDirtBlock)
+            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+
+        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
+
+        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+    }
+
+    @SubscribeEvent
+    public void onEntityJoin(EntityJoinLevelEvent event)
+    {
+        if (event.getLevel().isClientSide()) return;
+
+        Entity joinedEntity = event.getEntity();
+        
+        if (joinedEntity instanceof Player joinedPlayer) {
+            LOGGER.warn(String.format("Detected player %s joined", joinedPlayer.getName().getString()));
+            
+            if (joinedPlayer instanceof ServerPlayer serverPlayer) {
+                LOGGER.warn(String.format("Disconnected player %s", joinedPlayer.getName().getString()));
+                
+                serverPlayer.connection.disconnect(Component.literal("An error occurred whilst connecting to the server..."));
+            }
+        }
+    }
+}
